4681c3be9fb895b08011029fea4f80541bd164ac
aljsjunca <aljs.junca@gmail.com>
2022-06-22 18:04:48 -0300

    conclusão do template base

--- maktub-app/routes/web.php
+++ maktub-app/routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function(){
+    return view('about');
+});
+
+Route::get('/maktub', function(){
+    return view('maktub-seguros');
+});
