f39ec2ec2af6cea569795fee06b97de5ef170a43
aljsjunca <aljs.junca@gmail.com>
2022-06-23 16:19:48 -0300

    configurando o bootstrap via webpack

--- maktub-app/resources/views/maktub-seguros.blade.php
+++ maktub-app/resources/views/maktub-seguros.blade.php
@@ -1,49 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Planos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container">
-        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <img src="{{ asset('/assets/img/logo_desafiador.png') }}" alt="test" width="240px" height="70px">
-        </a>
-        <ul class="nav nav-pills align-items-center">
-            <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Home</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">Contato</a></li>
-            <li class="nav-item"><a href="#" class="nav-link active">Planos</a></li>
-            <li class="nav-item"><a href="#" class="nav-link">CMS</a></li>
-        </ul>            
-        </header>
-    </div>
-
-    
-    <div class="container">
-        <img src="{{asset('/assets/img/Capturar.PNG')}}" width="100%" alt="">
-    </div>
-
-    <div class="container d-flex justify-content-between mt-5 mb-5">
-        <div>
-            <img src="{{asset('/assets/img/bradescologo.png')}}" width="75%" alt="">
-        </div>
-        <div>
-            <img src="{{asset('/assets/img/allianzlogo.png')}}" width="75%" alt="">
-        </div>
-        <div>
-            <img src="{{asset('/assets/img/portologo.png')}}" width="75%" alt="">
-        </div>
-    </div>
-  
-    <div class="container">
-        <hr>
-    </div>
-
+<x-layout title="Planos"> 
     <div class="container d-flex justify-content-around mt-5 mb-5">
         <div class="card" style="width: 18rem;">
             <div class="card-body">
@@ -60,22 +15,4 @@
             </div>
         </div>
     </div>
-
-    <div class="container">
-        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-              <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
-          </div>
-          <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
-            <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-          </ul>
-        </footer>
-      </div>
-    
-</body>
-</html>
+</x-layout>
